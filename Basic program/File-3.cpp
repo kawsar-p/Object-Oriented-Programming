9dbb4f92b4544b5786e3408976fcaad500968200
Md. Kawsar Miah <mdkawsarmiah33@gmail.com>
2025-02-16 00:04:04 +0600

    Factorial

--- Basic program/File-3.cpp
+++ Basic program/File-3.cpp
@@ -1,1 +1,18 @@
-// 
+// Factorial value
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, flag =1 ;
+    cout << "Enter your value: ";
+    cin >> n;
+    
+    for(int i = 1; i <= n ; i++){
+    flag = flag * i;
+    }
+    
+    cout << "Factorial = " << flag << endl;
+
+    return 0;
+}
