7b602180a2f2fce087592a7c44e258e6b3134c48
Md. Kawsar Miah <mdkawsarmiah33@gmail.com>
2025-02-18 00:26:28 +0600

    Addition

---
+++ Function/File-1.cpp
@@ -0,0 +1,22 @@
+// Addition using function void type with perameter
+
+#include<iostream>
+using namespace std;
+
+void Addition(int a, int b){
+    int sum;
+    sum = a + b;
+    cout << "Addition: " << sum << endl;
+}
+
+int main(){
+    int a, b;
+    cout << "Enter your first value: ";
+    cin >> a;
+    cout << "Enter your second value: ";
+    cin >> b;
+    Addition(a,b);
+    
+    return 0;
+    
+}
